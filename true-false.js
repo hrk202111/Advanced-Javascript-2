b3774d4daa554786b4ecf52deba0377d95b4f585
hrk202111 <hrk202111@gmail.com>
2021-03-16 17:20:05 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,45 @@
+number er khetre
+const age = 4;              // age = -4 thakeleo condition true but age = 0 thakle conditon false dekhabe
+if(age){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
+
+
+// string er khetre
+const name = "solaiman"             // name = "solaiman" othoba name = " " thakle conditon true but name = "" thakle condition false
+if(name){  
+    console.log('conditon is true');
+}
+else{
+    console.log('condition is false');
+}
+
+// advanced learn
+let names = 0;
+if (name || name == 0){                  // akane 0 thakelo true abar onno songkha thakleo true
+    console.log(`condition is true`);
+}
+else{
+    console.log(`condition is false`);
+}
+
+
+// False 
+// 1. NaN
+// 2. false
+// 3. 0
+// 4. undefined
+// 5. null
+// 6. ""
+
+// Truthy
+// 1. " "
+// 2. {}
+// 3. []
+// 4. "0"
+// 5. "string er vitor kicu thaklei"
+
+
