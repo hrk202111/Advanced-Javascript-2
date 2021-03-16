86979db2e33f868f3cba47632a96c4312724111b
hrk202111 <hrk202111@gmail.com>
2021-03-16 17:49:25 +0600

    null vs undefined interview question learn

---
+++ null-vs-undefined.js
@@ -0,0 +1,27 @@
+let pakhi;
+console.log(pakhi); 
+
+
+function add(num1, num2) {
+    console.log(num1+num2);
+}
+const result = add(12, 13)
+console.log(result);
+
+
+function add(num1, num2) {
+    console.log(num1, num2);
+}
+const result = add(12)
+console.log(result);
+
+
+const premik = {name: 'smart dude', phone:5412013}
+console.log(premik.gf);
+
+let fun = undefined
+console.log(fun);
+
+
+let ages = [12, 25, 54]
+console.log(ages[12]);
